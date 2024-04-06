ab966d6d82102e987396ee7e7fe55366a5a809ed
yanshiqwq <yanshiqwq@outlook.com>
2024-04-07 02:28:50 +0800

    bug fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,27 @@
 import net.sourceforge.argparse4j.ArgumentParsers
 import net.sourceforge.argparse4j.inf.ArgumentParser
 import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
+import kotlin.io.path.Path
+import kotlin.io.path.exists
+import kotlin.io.path.name
+import kotlin.io.path.writeText
+
+// By: yanshiqwq (https://github.com/yanshiqwq)
+// Usage: java -jar {jar_path} -i {input_folder} -o {output_folder} -p {package_name}
+// Example: java -jar .\AiyatsbusPropertyGenerator-1.0-SNAPSHOT.jar -i F:\yanshiqwq\Project\AiyatsbusPropertyGenerator\data\bukkit\src\main\java\org\bukkit\ -o F:\yanshiqwq\Project\AiyatsbusPropertyGenerator\data\test -p org.bukkit
 
 fun main(args: Array<String>) {
     val parser = createArgumentParser()
     val namespace = parser.parseArgs(args)
 
-    val inputDirectory = namespace.getString("input")
-    val outputDirectory = namespace.getString("output")
+    var inputDirectory = namespace.getString("input")
+    var outputDirectory = namespace.getString("output")
+
+    if (!inputDirectory.endsWith("\\")) inputDirectory += "\\"
+    if (!outputDirectory.endsWith("\\")) outputDirectory += "\\"
     val packageName = namespace.getString("package")
 
     processInputAndOutput(inputDirectory, outputDirectory, packageName)
@@ -54,15 +67,15 @@ fun processInputAndOutput(inputDirectory: String, outputDirectory: String, packa
         // 读取 Java 文件
         val className = javaFile.nameWithoutExtension
         val javaCode = javaFile.readText()
-        val getterRegex = Regex("(get|is|has|can)[A-Z][a-zA-Z]*\\(\\)", RegexOption.MULTILINE)
-        val setterRegex =
-            Regex("(set)[A-Z][a-zA-Z]*\\((Boolean|Short|Int|Long|Float|Double|List)\\)", RegexOption.MULTILINE)
+        val getterRegex = Regex("(get|is|has|can)[A-Z][a-zA-Z]*\\(\\) \\{", RegexOption.MULTILINE)
+        val setterRegex = Regex("(set)[A-Z][a-zA-Z]*\\((Boolean|Short|Int|Long|Float|Double|List)\\) \\{", RegexOption.MULTILINE)
         val getterList = ArrayList<String>()
         val setterMap = mutableMapOf<String, String>()
         if (!getterRegex.containsMatchIn(javaCode)) continue
+
         val getterMatches = getterRegex.findAll(javaCode)
         for (it in getterMatches) {
-            val match = it.groups[0]!!.value
+            val match = it.groups[0]!!.value.removeSuffix(" {")
             if (listOf("isCancelled()", "getHandlers()", "getHandlerList()").contains(match)) continue
             val method = when {
                 match.startsWith("get") -> match.removePrefix("get")
@@ -72,25 +85,26 @@ fun processInputAndOutput(inputDirectory: String, outputDirectory: String, packa
             }.removeSuffix("()").replaceFirstChar { it.lowercase(Locale.getDefault()) }
             getterList.add(method)
         }
-
-        if (setterRegex.containsMatchIn(javaCode)) {
-            val setterMatches = setterRegex.findAll(javaCode)
-            setterMatches.forEach { it ->
-                val match = it.groups[0]!!.value.removeSuffix("()").split(" ")
-                val type = match[0]
-                val method = match[1].replaceFirstChar { it.lowercase(Locale.getDefault()) }.removePrefix("set")
-                setterMap[type] = method
+        try {
+            if (setterRegex.containsMatchIn(javaCode)) {
+                val setterMatches = setterRegex.findAll(javaCode)
+                setterMatches.forEach { it ->
+                    val match = it.groups[0]!!.value.removeSuffix("() {").split(" ")
+                    val type = match[0]
+                    val method = match[1].replaceFirstChar { it.lowercase(Locale.getDefault()) }.removePrefix("set")
+                    setterMap[type] = method
+                }
             }
-        }
+        } catch (ignored: IndexOutOfBoundsException) {} // wtf is this
 
-        val outputFile = File(
+        val outputFile = Path(
             javaFile.path.replace(inputDirectory.replace("\\\\", "\\"), outputDirectory.replace("\\\\", "\\"))
                 .replace(".java", ".kt")
         )
 
         if (!outputFile.exists() && getterList.isNotEmpty()) {
-            File(outputFile.parent).mkdirs()
-            outputFile.createNewFile()
+            outputFile.parent.toFile().mkdirs()
+            outputFile.toFile().createNewFile()
             println("Processing ${outputFile.name} ...")
             outputFile.writeText(generateKotlinPropertyClass(packageName, className, getterList, setterMap))
         }
@@ -103,7 +117,12 @@ fun camelCaseToHyphenated(text: String): String {
     }.lowercase(Locale.getDefault())
 }
 
-fun generateKotlinPropertyClass(packageName: String, className: String, getterList: ArrayList<String>, setterMap: Map<String, String>): String {
+fun generateKotlinPropertyClass(
+    packageName: String,
+    className: String,
+    getterList: ArrayList<String>,
+    setterMap: Map<String, String>
+): String {
     val classNameByHyphen = camelCaseToHyphenated(className)
     var getterCode = ""
     var setterCode = ""
@@ -135,8 +154,16 @@ fun generateKotlinPropertyClass(packageName: String, className: String, getterLi
         import taboolib.common.OpenResult
 
         /**
-         * Generated by AiyatsBusPropertyGenerator
+         * Aiyatsbus
+         * com.mcstarrysky.aiyatsbus.module.kether.property.bukkit.$className
+         * 
+         * @author yanshiqwq
+         * @since ${LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/M/d HH:mm"))}
+         * 
+         * # Generated by AiyatsBusPropertyGenerator #
+         * 
          */
+         
         @AiyatsbusProperty(
             id = "$classNameByHyphen",
             bind = $className::class
@@ -145,24 +172,26 @@ fun generateKotlinPropertyClass(packageName: String, className: String, getterLi
 
             override fun readProperty(instance: $className, key: String): OpenResult {
                 val property: Any? = when (key) {
-                    ${getterCode}else -> OpenResult.failed()
+                    $getterCode
+                    else -> OpenResult.failed()
                 }
                 return OpenResult.successful(property)
             }
 
             override fun writeProperty(instance: $className, key: String, value: Any?): OpenResult {
                 ${
-                    if (setterMap.isEmpty()) {
-                        "return OpenResult.failed()"
-                    } else {
-                        """
+        if (setterMap.isEmpty()) {
+            "return OpenResult.failed()"
+        } else {
+            """
                             when (key) {
-                                ${setterCode}else -> return OpenResult.failed()
+                                $setterCode
+                                else -> return OpenResult.failed()
                             }
                             return OpenResult.successful()
                         """.trimIndent()
-                    }
-                }
+        }
+    }
             }
     """.trimIndent()
 }
